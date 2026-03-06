5e8ab541191c5d9d0f2b65acede9230f49a9a723
Edem Useinov <edemuseinov322@gmail.com>
2026-03-06 14:20:06 +0300

    "Add ship display on screen"

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -16,8 +17,7 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # Назначение цвета фона
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -28,7 +28,8 @@ class AlienInvasion:
                     sys.exit()
 
             # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
