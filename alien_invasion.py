c841c44f894a80f214ebfabcc149d684e887d373
Edem Useinov <edemuseinov322@gmail.com>
2026-03-19 15:52:00 +0300

    Add bullet-alien collision detection

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,18 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Обработка коллизий снарядов, участвующих в коллизиях."""
+        # Удаление снарядов и пришельцев, участвующих в коллизиях.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Уничтожение существующих снарядов и создание нового флота.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _create_fleet(self):
         """Создания флота вторжения"""
         # Создание пришельца и вычисление количества пришельцев в ряду.
